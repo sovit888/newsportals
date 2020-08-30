d8a7ee990a01b8332497d4e956d8fa0b13317c43
sovit88 <65994487+sovit888@users.noreply.github.com>
2020-08-30 12:42:17 +0545

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.json());
 app.use("/post", require("./routes/postroute"));
 app.use("/comments", require("./routes/commentroute"));
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("newsportal/build"));
+  app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "newsportal", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 app.listen(PORT);
